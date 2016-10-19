f2f8c61876928543a334243b3852c1a1c27b6d3c
Harsh Panchal <hmpanchal95@gmail.com>
2016-10-19 11:48:17 +0530

    Add files via upload
    A simple recursive descent parser using Python

---
+++ rdparser.py
@@ -0,0 +1,32 @@
+p = 0
+flag = True
+
+def E():
+    global p
+    if string[p]=='i':
+        match('i')
+        Ed()
+
+def Ed():
+    global p
+    if string[p]=='+':
+        match('+')
+        match('i')
+        Ed()
+    else:
+        return
+
+def match(t):
+    global p
+    global flag
+    if string[p]==t:
+        p+=1
+    else:
+        flag = False
+
+string = raw_input("Enter the string: ")
+E()
+if (string[p]=='$' and flag):
+    print "Accepted!"
+else:
+    print "Rejected!"
